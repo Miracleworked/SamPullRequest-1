0dc644afdbc5963fe38deba2845bb708c340227e
Miracleworked <50160080+Miracleworked@users.noreply.github.com>
2019-05-21 15:42:01 -0700

    Update debug.py

--- debug.py
+++ debug.py
@@ -9,3 +9,18 @@ def lone_sum(a, b, c):
         return 0
     else:
         return a+b+c
+
+#this the fixed code
+    #There are 3 integer values a, b, and c and 
+def lone_sum(a, b, c): 
+    if a == b and a == c and b == c: 
+        return 0 
+    elif a >= b: 
+        return c 
+    elif b == c:
+        return a
+    elif a == c: 
+     return c
+    else:
+        return a+b+c
+print(lone_sum(3,5,7)
