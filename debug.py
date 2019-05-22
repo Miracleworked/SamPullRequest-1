23900d32e6297c2d80c1bf45af7ffc4d41b577ab
Miracleworked <50160080+Miracleworked@users.noreply.github.com>
2019-05-21 18:31:27 -0700

    Update debug.py
    https://github.com/gregorykremler/codingbat-python/blob/master/logic2/logic2.py
    
    https://stackoverflow.com/questions/15552449/sum-of-a-b-and-c-without-counting-doublet-values

--- debug.py
+++ debug.py
@@ -11,16 +11,16 @@ def lone_sum(a, b, c):
         return a+b+c
 
 #this the fixed code
-    #There are 3 integer values a, b, and c and 
-def lone_sum(a, b, c): 
-    if a == b and a == c and b == c: 
+    #There are 3 integer values a, b, and c. Two of the values is equal to the other
+def lone_sum(a, b, c):  # definition of lone_sum
+    if a == b and a == c and b == c:  #if all values equal each other return 0
         return 0 
-    elif a >= b: 
+    elif a >= b:     # if a is greater than/equal to b, return value of c
         return c 
-    elif b == c:
+    elif b == c:     #if b is equal to c, return value of a
         return a
-    elif a == c: 
+    elif a == c:     #elseif a equal c, return c
      return c
-    else:
+    else:            #else if all values fail, return the sum of a,b and c
         return a+b+c
-print(lone_sum(3,5,7)
+print(lone_sum(3,5,7)  #enter values to check code, print sum is 15
